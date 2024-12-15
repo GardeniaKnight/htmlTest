e67ffd0d0f4c81b0c0c42d6ed258da97ffea346a
Zibang Nie <2542881@dundee.ac.uk>
2024-12-15 22:38:23 +0800

    polish customer main page

--- script.js
+++
@@ -1,4 +0,0 @@
-// This script adds interactivity to the Learn More button
-document.getElementById("ctaButton").addEventListener("click", function() {
-    alert("Thank you for clicking! Explore more about the new iPhone.");
-  });
